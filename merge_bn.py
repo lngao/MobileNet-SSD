faaab7828ef043992df550b729bd195a047d67f4
lngao <gaolining2006@126.com>
2018-01-17 14:29:04 +0800

    modifed th mege_bn.py

--- merge_bn.py
+++ merge_bn.py
@@ -1,13 +1,13 @@
 import numpy as np  
 import sys,os  
-caffe_root = '/home/yaochuanqi/ssd/caffe/'
+caffe_root = '/home/haoweilai/caffe/'
 sys.path.insert(0, caffe_root + 'python')  
 import caffe  
 
-train_proto = 'MobileNetSSD_train.prototxt'  
-train_model = 'mobilenet_iter_73000.caffemodel'  #should be your snapshot caffemodel
+train_proto = './example/MobileNetSSD_train.prototxt'  
+train_model = './snapshot/mobilenet_iter_54000.caffemodel'  #should be your snapshot caffemodel
 
-deploy_proto = 'MobileNetSSD_deploy.prototxt'  
+deploy_proto = './example/MobileNetSSD_deploy.prototxt'  
 save_model = 'MobileNetSSD_deploy.caffemodel'
 
 def merge_bn(net, nob):
